23aa88e515ac6669074be559fa8bfe4510fdb71e
Jiward Torres <jiwardtorres@yahoo.com>
2025-08-11 11:39:53 -0700

    new

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,11 +4,11 @@ import { DisappearingFeatures } from "./DisappearingFeatures";
 export default function HomePage() {
   return (
     <>
-      {/* Your other home page content */}
+    
 
       <DisappearingFeatures />
 
-      {/* More sections if you want */}
+      
     </>
   );
 }
