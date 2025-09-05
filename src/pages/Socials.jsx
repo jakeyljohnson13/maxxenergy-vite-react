bda4f41ab906b815b4d2fc85e97d9031bed0e846
jakeyljohnson13 <jakeyl.johnson13@yahoo.com>
2025-09-05 12:01:52 -0700

    Modified socials logos

--- src/pages/Socials.jsx
+++ src/pages/Socials.jsx
@@ -1,3 +1,7 @@
+import inLogo from '../assets/in.png';
+import fbLogo from '../assets/fbb.png';
+import instaLogo from '../assets/insta.png';
+import xLogo from '../assets/xx.png';
 
 export default function Socials() {
     return(
@@ -9,18 +13,18 @@ export default function Socials() {
 
         <div className="links-top">
             <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://www.linkedin.com/company/maxx-potential/" target="_blank" >
-            <img className="image" src="src\assets\in.png" width={250}/>
+            <img className="image" src={inLogo} width={250}/>
             </a>
             <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/maxxpotentialllc" target="_blank">
-            <img className="image" src="src\assets\fbb.png" width={250}/>
+            <img className="image" src={fbLogo} width={250}/>
             </a>
         </div>
         <div className="links-bot">
             <a href="https://www.instagram.com/maxxpotentialtech/?hl=en" target="_blank">
-                <img className="image" src="src\assets\insta.png" width={250} />
+                <img className="image" src={instaLogo} width={250} />
                 </a>
             <a href="https://twitter.com/intent/tweet?url=https://x.com/MaxxTechnology" target="_blank" >
-                <img className="image" src="src\assets\xx.png" width={250} />
+                <img className="image" src={xLogo} width={250} />
                 </a>
 
         </div>
